025535ab2737ac09c64f7959b4fef881d12306b4
nicsimem2 <nicolas_1_@hotmail.fr>
2017-12-02 19:51:56 -0500

    Exception on number entered by user
    Yep here it goes fams:

--- Properties/Menu/Menu.cs
+++ Properties/Menu/Menu.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using System.Collections.Generic;
 using FinalProject.Properties;
 
@@ -6,11 +7,18 @@ namespace FinalProject.Properties.Menu
 {
     public class Menu
     {
-    
+        private static void ValidateNumber(string std)
+        {
+            Regex regex = new Regex(@"^\d$");
+
+            if (!regex.IsMatch(std))
+                throw new InvalidNumberException(std);
+
+        }
         public void Start()
         {
-            int nbofsites;
-            int nbofships;
+            int nbofsites = 0;
+            int nbofships = 0;
             Console.WriteLine("                                                             ");
             Console.WriteLine("LOGICIEL DE GESTION DE DÉCHET TOXIQUE                        ");
             Console.WriteLine("-------------------------------------                        ");
@@ -20,30 +28,86 @@ namespace FinalProject.Properties.Menu
             Console.WriteLine("                                                             ");
             Console.WriteLine("-------------------------------------                        ");
 
-            Console.SetCursorPosition(23, 3);
-            nbofsites = Convert.ToInt16(Console.ReadLine());
 
+            Console.SetCursorPosition(23, 3);
+
+            //apply regex
+            try
+            {
+             string data1 = Console.ReadLine();
+                ValidateNumber(data1);
+                nbofsites = Convert.ToInt32(data1);
+            }
+            catch (InvalidNumberException ex)
+            {
+                Console.Clear();
+                Console.WriteLine("                                                             ");
+                Console.WriteLine("LOGICIEL DE GESTION DE DÉCHET TOXIQUE                        ");
+                Console.WriteLine("-------------------------------------                        ");
+                Console.WriteLine("Nombre de site       : ____             PRESS ENTER WHEN DONE");
+                Console.WriteLine("                                                             ");
+                Console.WriteLine("Nombre de vaisseau   : ____                                  ");
+                Console.WriteLine("                                                             ");
+                Console.WriteLine("-------------------------------------                        ");
+                Console.WriteLine(ex.Message);
+
+                Console.ReadLine();
+
+                Console.Clear();
+
+                Start();
+            }
+
+            
+            
             Console.Clear();
 
             Console.WriteLine("                                                             ");
             Console.WriteLine("LOGICIEL DE GESTION DE DÉCHET TOXIQUE                        ");
             Console.WriteLine("-------------------------------------                        ");
-            Console.WriteLine("Nombre de site       : "+ nbofsites + "                          ");
+            Console.WriteLine("Nombre de site       : "+ nbofsites + "                      ");
             Console.WriteLine("                                                             ");
             Console.WriteLine("Nombre de vaisseau   : ____             PRESS ENTER WHEN DONE");
             Console.WriteLine("                                                             ");
             Console.WriteLine("-------------------------------------                        ");
             Console.SetCursorPosition(23, 5);
-            nbofships = Convert.ToInt16(Console.ReadLine());
+
+            //apply regex
+
+            try
+            {
+                string data3 = Console.ReadLine();
+                ValidateNumber(data3);
+                nbofships = Convert.ToInt32(data3);
+            }
+            catch (InvalidNumberException ex)
+            {
+                Console.Clear();
+                Console.WriteLine("                                                             ");
+                Console.WriteLine("LOGICIEL DE GESTION DE DÉCHET TOXIQUE                        ");
+                Console.WriteLine("-------------------------------------                        ");
+                Console.WriteLine("Nombre de site       : ____             PRESS ENTER WHEN DONE");
+                Console.WriteLine("                                                             ");
+                Console.WriteLine("Nombre de vaisseau   : ____                                  ");
+                Console.WriteLine("                                                             ");
+                Console.WriteLine("-------------------------------------                        ");
+                Console.WriteLine(ex.Message);
+
+                Console.ReadLine();
+
+                Console.Clear();
+
+                Start();
+            }
 
             Console.Clear();
 
             Console.WriteLine("                                                             ");
             Console.WriteLine("LOGICIEL DE GESTION DE DÉCHET TOXIQUE                        ");
             Console.WriteLine("-------------------------------------                        ");
-            Console.WriteLine("Nombre de site       : "+ nbofsites + "                          ");
+            Console.WriteLine("Nombre de site       : "+ nbofsites + "                      ");
             Console.WriteLine("                                                             ");
-            Console.WriteLine("Nombre de vaisseau   : "+ nbofships + "                         ");
+            Console.WriteLine("Nombre de vaisseau   : "+ nbofships + "                      ");
             Console.WriteLine("                                                             ");
             Console.WriteLine("-------------------------------------                        ");
             Console.WriteLine("Processing...                                                ");
